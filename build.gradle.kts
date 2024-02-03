7d31831ca6baa65258c1db380724a925710b5de0
Edward Muturi <edward.muturi@dlight.com>
2024-02-03 18:44:43 +0300

    - Add pre-commit and pre-push gradle task, - Add compose naming ktlint rule

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,12 @@ subprojects {
         debug.set(true)
     }
 }
+
+task("addPreCommitGitHookOnBuild") {
+    println("⚈ ⚈ ⚈ Running Add Pre Commit Git Hook Script on Build ⚈ ⚈ ⚈")
+    exec {
+        commandLine("cp", "./scripts/pre-commit", "./.git/hooks")
+//        commandLine("cp", "./scripts/pre-push", "./.git/hooks")
+    }
+    println("✅ Added Pre Commit Git Hook Script.")
+}
